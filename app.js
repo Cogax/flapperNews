dbd54608c3fb6b41839f0f4320d5548d14e97c6f
Andreas Gyr <andygyr@gmx.ch>
2015-01-06 00:52:44 +0100

    refractoring

--- app.js
+++ app.js
@@ -4,14 +4,14 @@
 angular.module('flapperNews', [])
  .controller('MainCtrl', ['$scope', function($scope) {
     $scope.posts = [
-      {'title': 'post1', 'content': 'content1', 'upvotes': 3},
-      {'title': 'post2', 'content': 'content2', 'upvotes': 2},
-      {'title': 'post3', 'content': 'content3', 'upvotes': 1}
+      {title: 'post1', content: 'content1', upvotes: 3},
+      {title: 'post2', content: 'content2', upvotes: 2},
+      {title: 'post3', content: 'content3', upvotes: 1}
     ];
 
     $scope.addPost = function() {
       if($scope.title === undefined || $scope.title === '') return;
-      $scope.posts.push({'title': $scope.title, 'content': $scope.content, 'upvotes': 0});
+      $scope.posts.push({title: $scope.title, content: $scope.content, upvotes: 0});
       $scope.title = '';
       $scope.content = '';
     }
