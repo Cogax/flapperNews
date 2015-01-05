4bf097d26b210f35b3744b625bfd10c2677a8dbb
Andreas Gyr <andygyr@gmx.ch>
2015-01-06 00:25:29 +0100

    add post button

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ angular.module('flapperNews', [])
       {'title': 'post2', 'content': 'content2', 'upvotes': 2},
       {'title': 'post3', 'content': 'content3', 'upvotes': 1}
     ];
+
+    $scope.addPost = function() {
+      $scope.posts.push({'title': 'post', 'content': 'content', 'upvotes': 0});
+    }
   }]);
