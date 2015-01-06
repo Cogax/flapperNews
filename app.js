dd9f6bcd8e916f555393576299bc1da1825184e0
Andreas Gyr <andygyr@gmx.ch>
2015-01-06 22:10:29 +0100

    switched to expressJS

--- app.js
+++ app.js
@@ -1,66 +1,60 @@
-/**
- * Created by andy on 05/01/15.
- */
-angular.module('flapperNews', ['ui.router'])
-  .config([
-    '$stateProvider',
-    '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-      $stateProvider
-        .state('home', {
-          url: '/home',
-          templateUrl: '/home.html',
-          controller: 'MainCtrl'
-        })
-        .state('post', {
-          url: '/post/{id}',
-          templateUrl: '/post.html',
-          controller: 'PostCtrl'
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
         });
+    });
+}
 
-      $urlRouterProvider.otherwise('home');
-    }
-  ])
-  .factory('posts', [function() {
-    var o = {
-      posts: []
-    }
-    return o;
-  }])
-  .controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
-    $scope.posts = posts.posts;
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
-    $scope.addPost = function() {
-      if ($scope.title === undefined || $scope.title === '') return;
-      $scope.posts.push({
-        title: $scope.title,
-        content: $scope.content,
-        upvotes: 0,
-        comments: []
-      });
-      $scope.title = '';
-      $scope.content = '';
-    }
 
-    $scope.incrementUpvote = function(post) {
-      post.upvotes++;
-    }
-  }])
-  .controller('PostCtrl', ['$scope', '$stateParams','posts', function($scope, $stateParams, posts) {
-    $scope.post = posts.posts[$stateParams.id];
-
-    $scope.addComment = function() {
-      if($scope.comment === undefined || $scope.comment === '') return;
-      $scope.post.comments.push({
-        author: $scope.author,
-        content: $scope.comment,
-        upvotes: 0
-      });
-      $scope.comment = '';
-      $scope.author = '';
-    }
-
-    $scope.incrementUpvote = function(comment) {
-      comment.upvotes++;
-    }
-  }]);
+module.exports = app;
